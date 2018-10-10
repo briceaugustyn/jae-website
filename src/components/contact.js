ba1d2cac1e4d3078e69c126d6be8f4cc2a47887f
Brice Augustyn <baugustyn1000@gmail.com>
2018-10-10 16:13:24 -0700

    add link for phone number

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,6 @@ const encode = (data) => {
     .join("&");
 }
 
-
 class Contact extends React.Component {
 
   constructor(props) {
@@ -31,12 +30,7 @@ class Contact extends React.Component {
     this.setState({ message: '' });
   };
 
-
-
-
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-
 
   render() {
     const { name, email, message } = this.state;
@@ -51,15 +45,11 @@ class Contact extends React.Component {
                 <h2>Get in touch with us!</h2>
                 <div className="line-shape" />
               </div>
-              {/* <div className="footer-text wow fadeInUp" data-wow-offset={200} data-wow-duration=".8s" data-wow-delay=".2s">
-                <p>We'll send you epic weekly blogs, whitepapers and things to make your app startup thrive, all
-                  FREE!</p> */}
-              {/* </div> */}
               <div className="address-text wow fadeInUp" data-wow-offset={200} data-wow-duration=".8s" data-wow-delay=".2s">
                 <p><span>Address :</span> Santa Rosa , California</p>
               </div>
               <div className="phone-text wow fadeInUp" data-wow-offset={200} data-wow-duration=".8s" data-wow-delay=".2s">
-                <p><span>Phone: </span> 1 (888) 759 2786</p>
+                <p><span>Phone: </span><a href="tel:8887592786">1 (888) 759 2786</a></p>
               </div>
               <div className="email-text wow fadeInUp" data-wow-offset={200} data-wow-duration=".8s" data-wow-delay=".2s">
                 <p><span>Email: </span><a href="mailto:jason.lee@adaptivequalitysystems.com?subject=Request to ADQS&body=Hello,">jason.lee@adaptivequalitysystems.com</a></p>
@@ -68,53 +58,9 @@ class Contact extends React.Component {
             </div>
             <div className="col-md-6">
 
-
-
-
-
-              {/* <form action method="post" id="contactForm" name="contactForm" onSubmit={this.handleSubmit}>
-<fieldset>
-      <div>
-        <label htmlFor="contactName">Name <span className="required">*</span></label>
-        {/* <input type="text" defaultValue size={35} id="contactName" name="contactName" /> */}
-              {/* <input type="text" defaultValue size={35} id="contactName" name="name" value={name} onChange={this.handleChange} />
-      </div>
-      <div> */}
-              {/* <label htmlFor="contactEmail">Email <span className="required">*</span></label> */}
-              {/* <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" /> */}
-              {/* <input type="email" name="email" value={email} onChange={this.handleChange} />
-      </div>
-      <div>
-        <label htmlFor="contactSubject">Subject</label>
-        <input type="text" size={35} id="contactSubject" name="contactSubject" />
-      </div>
-      <div>
-        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-        <textarea cols={50} rows={15} id="contactMessage"  defaultValue={""} name="message" value={message} onChange={this.handleChange}/>
-
-      </div> */}
-              {/* <div>
-        <button className="submit" type="submit">Submit</button>
-        <span id="image-loader">
-          <img alt src="images/loader.gif" />
-        </span>
-      </div> */}
-              {/* <div>
-      <button type="submit">Send</button>
-      <span id="image-loader">
-          <img alt src="images/loader.gif" />
-        </span>
-    </div>
-    </fieldset>
-    </form>  */}
-              {/* Form End */}
-
-
-
               {/* Form Start*/}
               <div className="contact_from">
                 <form action method="post" id="contactForm" name="contactForm" onSubmit={this.handleSubmit}>
-                  {/* <form action="#" method="post" onSubmit={this.handleSubmit}> */}
                   {/* Message Input Area Start */}
                   <div className="contact_input_area">
                     <div className="row">
@@ -136,11 +82,6 @@ class Contact extends React.Component {
                       <div className="col-12 wow fadeInRight" data-wow-offset={200} data-wow-duration=".8s" data-wow-delay=".2s">
                         <div className="form-group">
                           <textarea name="message" className="form-control" id="message" cols={30} rows={4} placeholder="Your Message *" required defaultValue={""} value={message} onChange={this.handleChange} />
-                          {/* <label htmlFor="contactMessage">Message <span className="required">*</span></label> */}
-                          {/* <textarea cols={50} rows={15} id="contactMessage" defaultValue={""} name="message" value={message} onChange={this.handleChange} /> */}
-
-
-                          {/* <textarea cols={50} rows={15} id="contactMessage"  defaultValue={""} name="message" value={message} onChange={this.handleChange}/> */}
                         </div>
                       </div>
                       {/* Single Input Area Start */}
@@ -148,24 +89,15 @@ class Contact extends React.Component {
                         <button type="submit" className="btn submit-btn">Send Now</button>
                       </div>
 
-
                     </div>
                   </div>
                   {/* Message Input Area End */}
                 </form>
-
-                <form action method="post" id="contactForm" name="contactForm" onSubmit={this.handleErase}></form>
-                <div className="col-12 wow fadeInRight" data-wow-offset={200} data-wow-duration=".8s" data-wow-delay=".2s">
-                  <button type="submit" className="btn submit-btn">Erase</button>
-                </div>
-
-
               </div>
             </div>
           </div>
         </div>
       </section>
-
 
     );
   }
