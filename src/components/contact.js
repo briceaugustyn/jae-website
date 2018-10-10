b73a319d21bda549118f5588205ce51faf800071
Brice Augustyn <baugustyn1000@gmail.com>
2018-10-10 09:28:41 -0700

    first email attempt

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,38 @@
+
 import React from 'react';
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+
 class Contact extends React.Component{
-    render() {
-        
+  
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Email Sent To Brice"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
+  
+  render() {
+    const { name, email, message } = this.state;  
       return (
 
 <section className="footer-contact-area section_padding_100 clearfix" id="contact">
@@ -29,6 +59,50 @@ class Contact extends React.Component{
               </div>
             </div>
             <div className="col-md-6">
+
+
+
+
+
+<form action method="post" id="contactForm" name="contactForm" onSubmit={this.handleSubmit}>
+<fieldset>
+      <div>
+        <label htmlFor="contactName">Name <span className="required">*</span></label>
+        {/* <input type="text" defaultValue size={35} id="contactName" name="contactName" /> */}
+        <input type="text" defaultValue size={35} id="contactName" name="name" value={name} onChange={this.handleChange} />
+      </div>
+      <div>
+        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+        {/* <input type="text" defaultValue size={35} id="contactEmail" name="contactEmail" /> */}
+        <input type="email" name="email" value={email} onChange={this.handleChange} />
+      </div>
+      <div>
+        <label htmlFor="contactSubject">Subject</label>
+        <input type="text" size={35} id="contactSubject" name="contactSubject" />
+      </div>
+      <div>
+        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+        <textarea cols={50} rows={15} id="contactMessage"  defaultValue={""} name="message" value={message} onChange={this.handleChange}/>
+
+      </div>
+      {/* <div>
+        <button className="submit" type="submit">Submit</button>
+        <span id="image-loader">
+          <img alt src="images/loader.gif" />
+        </span>
+      </div> */}
+      <div>
+      <button type="submit">Send</button>
+      <span id="image-loader">
+          <img alt src="images/loader.gif" />
+        </span>
+    </div>
+    </fieldset>
+    </form> 
+{/* Form End */}
+
+
+
               {/* Form Start*/}
               <div className="contact_from">
                 <form action="#" method="post">
